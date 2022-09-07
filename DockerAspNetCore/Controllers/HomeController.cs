c8c3181d6964ab194d7fcc3113c17d0dd4302629
Fatih <ffatihsafak@gmail.com>
2022-09-08 00:29:32 +0300

    controller

--- DockerAspNetCore/Controllers/HomeController.cs
+++ DockerAspNetCore/Controllers/HomeController.cs
@@ -1,9 +1,12 @@
 ﻿using DockerAspNetCore.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,14 +15,46 @@ namespace DockerAspNetCore.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IFileProvider _fileProvider;
+        public HomeController(ILogger<HomeController> logger, IFileProvider fileProvider)
         {
             _logger = logger;
+            _fileProvider = fileProvider;
         }
 
         public IActionResult Index()
         {
+            return View();
+        }
+        public IActionResult ImageShow()
+        {
+            var images = _fileProvider.GetDirectoryContents("wwwroot/images").ToList().Select(x => x.Name);
+            return View(images);
+        }
+        [HttpPost]
+        public IActionResult ImageShow(string name)
+        {
+            var file = _fileProvider.GetDirectoryContents("wwwroot/images").ToList().First(x => x.Name == name);
+            System.IO.File.Delete(file.PhysicalPath);
+            return RedirectToAction("ImageShow");
+        }
+        [HttpGet]
+        public IActionResult ImageSave()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> ImageSave(IFormFile imageFile)
+        {
+            if(imageFile!=null && imageFile.Length > 0)
+            {
+                var fileName = Guid.NewGuid().ToString() + Path.GetExtension(imageFile.FileName);
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images", fileName);
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await imageFile.CopyToAsync(stream);
+                } ;
+            }
             return View();
         }
 
